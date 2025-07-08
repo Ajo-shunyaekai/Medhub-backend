17dd810a2c255141e9dabc36dbe6e50f49cdc1b5
Ajo-shunyaekai <Ajo@shunyaekai.tech>
2025-07-08 13:34:36 +0530

    added: s3 bucket proxy

--- app.js
+++ app.js
@@ -18,6 +18,30 @@ const { rateLimiter } = require("./middleware/expressRateLimiter");
 const { corsOptions } = require("./config/corsOptions");
 // require('./schedulers/tasks');
 
+const https = require("https");
+const http = require("http");
+const { URL } = require("url");
+
+//proxy
+app.get("/pdf-proxy/*", async (req, res) => {
+  const filename = decodeURIComponent(req.params[0]); // Get full path after /pdf-proxy/
+
+  const s3Url = `https://medhubglobal.s3.ap-south-1.amazonaws.com/testing/${filename}`;
+  const parsedUrl = new URL(s3Url);
+
+  const protocol = parsedUrl.protocol === "https:" ? https : http;
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  protocol.get(s3Url, (s3Res) => {
+    res.setHeader("Content-Type", s3Res.headers["content-type"] || "application/pdf");
+    s3Res.pipe(res);
+  }).on("error", (err) => {
+    console.error("Proxy error:", err);
+    res.status(500).send("Error fetching PDF from S3.");
+  });
+});
+
 // db-connection
 connect();
 
